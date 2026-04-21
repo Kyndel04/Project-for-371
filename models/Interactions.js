a3d5ab171cc2a4e325d20312da30b216a0504f12
Kyndel04 <156946490+Kyndel04@users.noreply.github.com>
2026-04-21 22:28:31 +0000

    Adding the view feature where it will save in a data base

--- models/Interactions.js
+++ models/Interactions.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //Should create a collection for analytics for movies and user interactions (likes/dislikes)
 const interactionSchema = new mongoose.Schema({
     movieTitle: { type: String, required: true },
-    action: { type: String, enum: ['like', 'dislike'], required: true },
+    action: { type: String, enum: ['like', 'dislike', 'views'], required: true },
     timestamp: { type: Date, default: Date.now }
 })
 
