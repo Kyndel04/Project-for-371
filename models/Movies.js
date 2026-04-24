de741504eb3fb8f0e208f45db2c675933559ed52
Kyndel04 <156946490+Kyndel04@users.noreply.github.com>
2026-04-24 02:46:25 +0000

    added posters and video url to sheama of the database storage

--- models/Movies.js
+++ models/Movies.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
 
 const movieSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    posterUrl: { type: String, required: true }
-})
+    posterUrl: { type: String, required: true },
+    description: { type: String, default: "No description available." },
+    videoUrl: { type: String, required: true }
+});
 
 module.exports = mongoose.model('Movie', movieSchema);
